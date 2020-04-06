b1ca2b753f4a19a0e04a441ce6f063ea8a537308
Jocelyn <59553647+JocelynMilRodriguez@users.noreply.github.com>
2020-04-06 05:13:06 -0400

    Update ViewController.swift

--- Homework1_TurkeyRoasting/ViewController.swift
+++ Homework1_TurkeyRoasting/ViewController.swift
@@ -1,10 +1,12 @@
 //  ViewController.swift
-//
-//Homework1_TurkeyRoasting
-//
+//  Homework1_TurkeyRoasting
 //  Created by Jocelyn M Rodriguez on 4/5/20.
 //  Copyright © 2020 CUNY Borough of Manhattan Community College. All rights reserved.
-//
+//  Input:  No User Input 
+//  Output: Roasting time
+//  Purpose: To determine the time it will take to roast turkey based on it's weight 
+
+
 import UIKit
 
 class ViewController: UIViewController
@@ -33,11 +35,12 @@ class ViewController: UIViewController
         }
             
         // the turkey must weight greater or equal to 12 and less than or equal to 16 lbs to excute
-        else if turkeyWeightInPounds >= 12 && turkeyWeightInPounds <= 16 {
+        else if turkeyWeightInPounds >= 12 && turkeyWeightInPounds <= 16 
+        {
             textView.text = "\n\n\n\t For a \(turkeyWeightInPounds)lb turkey it would take from 2hrs and 15 mins to 3hrs and 15mins to roast "
         }
         
-        // excute if all other condition cannot be met
+        // excute if all other conditions cannot be met
         else
         {
             textView.text = "\n\n\n\t This \(turkeyWeightInPounds)lb turkey is either too small to feed everyone or too big to fit in the oven."
